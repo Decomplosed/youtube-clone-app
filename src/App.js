44f9b6b162372b6e3dcb06a66e4aaee6c5e6b07c
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-28 12:34:19 +0100

    created base template with Grid

--- src/App.js
+++ src/App.js
@@ -6,7 +6,23 @@ import youtube from './api/youtube'
 
 class App extends React.Component {
   render() {
-    return <div className='App'></div>
+    return (
+      <Grid container justify='center' spacing={16}>
+        <Grid item xs={11}>
+          <Grid container spacing={16}>
+            <Grid item xs={12}>
+              {/* {SEARCH BAR} */}
+            </Grid>
+            <Grid item xs={8}>
+              {/* {VIDEO DETAILS} */}
+            </Grid>
+            <Grid item xs={4}>
+              {/* {VIDEO LIST} */}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+    )
   }
 }
 
