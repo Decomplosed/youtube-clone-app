40c0d0db8503584943bf9ab953092ffe099d4810
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-28 12:40:23 +0100

    changed VideoDetails to be functional component

--- src/components/VideoDetails.js
+++ src/components/VideoDetails.js
@@ -1,9 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class VideoDetails extends Component {
-  render() {
-    return <h1>VideoDetails component</h1>
-  }
+const VideoDetails = () => {
+  return <h1>VideoDetails component</h1>
 }
 
 export default VideoDetails
