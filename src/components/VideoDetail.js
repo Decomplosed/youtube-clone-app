46c77bc6153890fd4a58f126b76a43c18114b470
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-29 08:59:17 +0200

    added iframe to the template

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,7 +5,15 @@ import { Paper, Typography } from '@material-ui/core'
 const VideoDetail = ({ video }) => {
   return (
     <React.Fragment>
-      <Paper elevation={6} style={{ height: '70%' }}></Paper>
+      <Paper elevation={6} style={{ height: '70%' }}>
+        <iframe
+          frameBorder='0'
+          height='100%'
+          width='100%'
+          title='Video Player'
+          src=''
+        />
+      </Paper>
       <Paper elevation={6} style={{ padding: '15px' }}></Paper>
     </React.Fragment>
   )
