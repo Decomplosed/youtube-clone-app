127fd830cc5c56ac86649f8da82babacdb52cd53
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-29 09:24:57 +0200

    pass videos to the VideoList component as a prop

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
               <VideoDetail video={selectedVideo} />
             </Grid>
             <Grid item xs={4}>
-              <VideoList />
+              <VideoList videos={videos} />
             </Grid>
           </Grid>
         </Grid>
