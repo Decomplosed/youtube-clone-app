5a2d0e0757f8461fc3d70146c66d6daa95a2c353
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-29 09:01:42 +0200

    fetch the url

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,6 +5,8 @@ import { Paper, Typography } from '@material-ui/core'
 const VideoDetail = ({ video }) => {
   if (!video) return <div>Loading...</div>
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
   return (
     <React.Fragment>
       <Paper elevation={6} style={{ height: '70%' }}>
