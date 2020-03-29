5ebb620b38d6709dd64fd00882ffe2c5a752e646
Bart Frackiewicz <decomplosed@gmail.com>
2020-03-29 09:46:36 +0200

    extracted onVideoSelect from props

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Grid, Paper, Typography } from '@material-ui/core'
 
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <Grid item xs={12}>
       <Paper style={{ display: 'flex', alignItems: 'center' }}>
